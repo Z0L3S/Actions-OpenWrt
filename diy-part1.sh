83e8b167aa8b65408c48e8043c3a0d70a34d6e5a
Z0L3S <93368816+Z0L3S@users.noreply.github.com>
2022-07-24 01:04:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+# sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+# sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 # sed -i '$a src-git modeminfo https://github.com/koshev-msk/luci-app-modeminfo.git' feeds.conf.default
 # sed -i '$a src-git atinout https://github.com/4IceG/luci-app-atinout-mod' feeds.conf.default
 # sed -i '$a src-git 3ginfo https://github.com/4IceG/luci-app-3ginfo' feeds.conf.default
